26ab3009be9f534b8df6cd312b869458b27116eb
JjungminLee <julie0964@naver.com>
2023-07-23 13:12:50 +0900

    feat:프로젝트 세팅

---
+++ src/main/java/com/onna/onnaback/OnnaBackApplication.java
@@ -0,0 +1,13 @@
+package com.onna.onnaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnnaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnnaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onna/onnaback/OnnaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onna.onnaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnnaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
