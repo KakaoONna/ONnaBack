1666befece55c188485776428ac9058479fbdfb4
JjungminLee <julie0964@naver.com>
2023-08-31 22:44:03 +0900

    refactor:CustomUserDetails custom

---
+++ src/main/java/com/onna/onnaback/global/oauth/application/service/CustomUserDetails.java
@@ -0,0 +1,69 @@
+package com.onna.onnaback.global.oauth.application.service;
+
+import com.onna.onnaback.domain.member.domain.Member;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+@Data
+public class CustomUserDetails implements UserDetails {
+
+
+    private Member member;
+
+    public CustomUserDetails(Member member){
+        this.member=member;
+    }
+
+    public Long getMemberId(){
+        return member.getMemberId();
+    }
+
+    public String getEmail(){
+        return member.getEmail();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return member.getRole().toString();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
