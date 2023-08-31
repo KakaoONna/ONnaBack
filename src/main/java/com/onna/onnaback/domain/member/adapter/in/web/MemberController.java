73fd8dc7cc90bb226d40a122549c13b05d4dda34
JungminLee <85864699+JjungminLee@users.noreply.github.com>
2023-08-31 22:46:28 +0900

    refactor : CustomUserDetails 커스텀 (#40)
    * fix:소셜 로그인 수정
    
    * fix:소셜 로그인 로직
    
    * fix:소셜로그인 jwt filter수정
    
    * fix:transaction 짜잘짜잘
    
    * feat:검색 API개발
    
    * fix:코멘트 맞춰 수정
    
    * refactor:CustomUserDetails custom

--- src/main/java/com/onna/onnaback/domain/member/adapter/in/web/MemberController.java
+++ src/main/java/com/onna/onnaback/domain/member/adapter/in/web/MemberController.java
@@ -1,5 +1,8 @@
 package com.onna.onnaback.domain.member.adapter.in.web;
 
+import com.onna.onnaback.domain.member.domain.Member;
+import com.onna.onnaback.global.oauth.application.service.CustomUserDetails;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -11,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
     @GetMapping("/test")
-    public String index() {
-        return "Hello World";
+    public ResponseEntity<Member> index(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+
+        return ResponseEntity.ok().body(Member.builder().memberId(customUserDetails.getMemberId())
+                .name(customUserDetails.getUsername())
+                .email(customUserDetails.getEmail()).build());
     }
 }
