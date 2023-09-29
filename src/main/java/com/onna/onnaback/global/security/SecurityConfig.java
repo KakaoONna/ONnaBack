8c78444d617b7bcd2c313db153918722b7221413
JjungminLee <julie0964@naver.com>
2023-09-29 23:17:04 +0900

    fix:add health check

--- src/main/java/com/onna/onnaback/global/security/SecurityConfig.java
+++ src/main/java/com/onna/onnaback/global/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                 // [PART 2]
                 //== URL별 권한 관리 옵션 ==//
                 .authorizeRequests()
-                .antMatchers("/swagger-ui/**","/v3/api-docs", "/swagger-resources/**","/api/auth/**").permitAll()
+                .antMatchers("/swagger-ui/**","/v3/api-docs", "/swagger-resources/**","/api/auth/**","/").permitAll()
                 .anyRequest().authenticated()// 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
                 .and()
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
