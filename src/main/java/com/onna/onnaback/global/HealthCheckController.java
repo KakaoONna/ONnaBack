9adfdf77be5a6e9c82759627fcf2fdab9a978dae
JjungminLee <julie0964@naver.com>
2023-09-29 23:15:46 +0900

    feat:https설정

--- src/main/java/com/onna/onnaback/global/HealthCheckController.java
+++ src/main/java/com/onna/onnaback/global/HealthCheckController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-
     @GetMapping("/")
     public String HealthCheck(){
         return "";
