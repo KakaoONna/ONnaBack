b9ef7aa8154cce03daf826493b90ae715ae6a4aa
JjungminLee <julie0964@naver.com>
2023-10-06 22:40:51 +0900

    add:cors 설정 추가

--- src/main/java/com/onna/onnaback/global/security/SecurityConfig.java
+++ src/main/java/com/onna/onnaback/global/security/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000","https://kakao-onna.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
